14458a161a99f1b3680e54a39df32ca09a05a588
Andreas Mayer <andreas.mayer@3bankenit.at>
2024-06-13 10:04:40 +0200

    Initial commit.

---
+++ src/main/java/org/printassist/jmbackend/JobManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package org.printassist.jmbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobManagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobManagerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/printassist/jmbackend/JobManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.printassist.jmbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobManagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
