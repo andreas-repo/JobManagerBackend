50198894292c26c3ae2ac081ea3fd4d832462503
Andreas Mayer <andreas.mayer@3bankenit.at>
2024-08-07 08:59:21 +0200

    -- built ResourceProvider class to provide values from application.yml -- implemented values in application.yml -- changed EmailSenderServiceImpl to uses values from application.yml

--- src/main/java/org/printassist/jmbackend/services/EmailSenderServiceImpl.java
+++ src/main/java/org/printassist/jmbackend/services/EmailSenderServiceImpl.java
@@ -9,6 +9,8 @@ import jakarta.mail.internet.MimeBodyPart;
 import jakarta.mail.internet.MimeMessage;
 import jakarta.mail.internet.MimeMultipart;
 import org.printassist.jmbackend.controllers.models.Email;
+import org.printassist.jmbackend.providers.ResourceProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -16,49 +18,47 @@ import java.util.Properties;
 
 @Service
 public class EmailSenderServiceImpl {
-
+    private ResourceProvider resourceProvider;
     private static final Properties PROPERTIES = new Properties();
-    private static final String USERNAME = "xerox6655@gmx.at";
-    private static final String PASSWORD = "gfMe45xhAh9B4Zp"; //TODO set password
-    private static final String HOST = "mail.gmx.net";
-    private static final String PORT = "587";
     private static final boolean DEBUG = true;
     private static final String CONTENT_TYPE = "text/html";
 
-    static {
+	public EmailSenderServiceImpl(ResourceProvider resourceProvider) {
+		this.resourceProvider = resourceProvider;
+	}
+
+	public Email sendEmail(Email email) {
+        PROPERTIES.put("mail.smtp.user", resourceProvider.getUser());
+        PROPERTIES.put("mail.smtp.password", resourceProvider.getPassword());
+        PROPERTIES.put("mail.smtp.host", resourceProvider.getHost());
+        PROPERTIES.put("mail.smtp.port", resourceProvider.getPort());
         PROPERTIES.put("mail.debug", "true");
         PROPERTIES.put("mail.transport.protocol", "smtp");
-        PROPERTIES.put("mail.smtp.host", HOST);
-        PROPERTIES.put("mail.smtp.port", PORT);
         PROPERTIES.put("mail.smtp.auth", "true");
         PROPERTIES.put("mail.smtp.starttls.enable", "true");
-        PROPERTIES.put("mail.smtp.user", USERNAME);
-        PROPERTIES.put("mail.smtp.password", PASSWORD);
         PROPERTIES.put("mail.smtp.isSSL", "false");
-        PROPERTIES.setProperty("mail.smtp.ssl.protocols", "TLSv1.1 TLSv1.2");
-    }
+        PROPERTIES.put("mail.smtp.ssl.protocols", "TLSv1.1 TLSv1.2");
 
-    public Email sendEmail(Email email) {
         Session session = Session.getInstance(PROPERTIES, new Authenticator()
         {
             @Override
             protected PasswordAuthentication getPasswordAuthentication()
             {
-                return new PasswordAuthentication(PROPERTIES.getProperty("mail.smtp.user"),
-                        PROPERTIES.getProperty("mail.smtp.password"));
+                return new PasswordAuthentication(resourceProvider.getUser(),
+                        resourceProvider.getPassword());
             }
         });
+
         session.setDebug(DEBUG);
 
         try {
             // create a message with headers
             MimeMessage msg = new MimeMessage(session);
-            msg.setFrom(new InternetAddress(USERNAME));
+            msg.setFrom(new InternetAddress(resourceProvider.getUser()));
             InternetAddress addressTo = new InternetAddress(email.getTo());
             msg.setRecipients(Message.RecipientType.TO, String.valueOf(addressTo));
             msg.setSubject(email.getSubject());
             msg.setSentDate(new Date());
-            //msg.setContent(email.getMessageBody(), CONTENT_TYPE);
 
             MimeMultipart multipart = new MimeMultipart();
             BodyPart messageBodyPart = new MimeBodyPart();
